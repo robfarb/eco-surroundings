f02fbc416b987130b429d1614212fdd54111e7d2
robfarb <rfarb@goodwatercap.com>
2023-03-22 22:42:36 -0700

    add basic html to homepage"

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/clear.css';
+import '../styles/common.css';
+import '../styles/carouFredSel.css'
+import '../styles/sm-clean.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
